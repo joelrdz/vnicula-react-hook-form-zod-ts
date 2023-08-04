f9d3ee0106b28c8d738bb21917899fd760ecfea2
Joel Rdz <joelrdzh@gmail.com>
2023-08-04 13:17:25 -0600

    useForm register and handleSubmit

--- pages/sign-up.tsx
+++ pages/sign-up.tsx
@@ -1,8 +1,16 @@
+import { useCallback } from 'react';
 import { Input } from 'react-daisyui';
+import { useForm } from 'react-hook-form';
 
 export default function SignUpPage() {
+  const { register, handleSubmit } = useForm();
+
+  const submitForm = useCallback((data: unknown) => {
+    console.log('form is valid', data);
+  }, []);
+
   return (
-    <div
+    <form
       style={{
         display: 'flex',
         flexFlow: 'column',
@@ -11,14 +19,16 @@ export default function SignUpPage() {
         justifyContent: 'center',
         height: '100vh',
       }}
+      onSubmit={handleSubmit(submitForm)}
     >
       <div className="form-control w-full max-w-xs">
         <label htmlFor="email" className="label">
           <span className="label-text">email</span>
         </label>
-        <Input color="ghost" id="email" type="text" />
+        <Input color="ghost" id="email" type="text" {...register('email')} />
         <span className="label-text text-error">An error</span>
       </div>
-    </div>
+      <button>Submit</button>
+    </form>
   );
 }
