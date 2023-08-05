cc4f1649d503f9ad1352d29506ce484c78fcd1de
Joel Rdz <joelrdzh@gmail.com>
2023-08-04 18:30:13 -0600

    TextField is reusable, adding extra fields

--- pages/sign-up.tsx
+++ pages/sign-up.tsx
@@ -29,10 +29,26 @@ export default function SignUpPage() {
     >
       <TextField
         id="email"
-        label="email"
+        label="Email"
         type="email"
         inputProps={register('email', { required: 'Email is required' })}
         error={errors.email?.message as string}
+      />
+      <TextField
+        id="password"
+        label="Password"
+        type="password"
+        inputProps={register('password', { required: 'Password is required' })}
+        error={errors.password?.message as string}
+      />
+      <TextField
+        id="confirmPassword"
+        label="Confirm Password"
+        type="password"
+        inputProps={register('confirmPassword', {
+          required: 'Confirm Password is required',
+        })}
+        error={errors.confirmPassword?.message as string}
       />
       <button>Submit</button>
     </form>
